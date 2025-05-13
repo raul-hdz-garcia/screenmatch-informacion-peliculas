07a0b91480d759979783440ad62c8db01e632582
Raul Hernandez <157054531+raul-hdz-garcia@users.noreply.github.com>
2025-05-12 19:20:00 -0600

    Add files via upload

---
+++ screenmatch-informacion-peliculas/src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -0,0 +1,72 @@
+package com.alura.screenmatch.principal;
+
+import com.alura.screenmatch.excepcion.ErrorEnConversionDeDuracionException;
+import com.alura.screenmatch.modelos.Titulo;
+import com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
+
+        while (true) {
+            System.out.println("Escriba el nombre de una película: ");
+            var busqueda = lectura.nextLine();
+
+            if (busqueda.equalsIgnoreCase("salir")) {
+                break;
+            }
+
+            String direccion = "https://www.omdbapi.com/?t=" + busqueda.replace(" ", "+") + "&apikey=6f9a14c4";
+
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(direccion))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+                TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println("Titulo: " + miTituloOmdb);
+
+                Titulo miTitulo = new Titulo(miTituloOmdb);
+                System.out.println("Titulo ya convertido: " + miTitulo);
+
+                titulos.add(miTitulo);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Ocurrió un error: ");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error en la URI, verifique la dirección.");
+            } catch (ErrorEnConversionDeDuracionException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println(titulos);
+
+        FileWriter escritura = new FileWriter("titulos.json");
+        escritura.write(gson.toJson(titulos));
+        escritura.close();
+        System.out.println("Finalizó la ejecución del programa!");
+
+    }
+}
---
+++ screenmatch-informacion-peliculas/src/com/alura/screenmatch/principal/PrincipalConListas.java
@@ -0,0 +1,52 @@
+package com.alura.screenmatch.principal;
+
+import com.alura.screenmatch.modelos.Pelicula;
+import com.alura.screenmatch.modelos.Serie;
+import com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class PrincipalConListas {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        miPelicula.evalua(9);
+        Pelicula otraPelicula = new Pelicula("Avatar", 2023);
+        otraPelicula.evalua(6);
+        var peliculaDeBruno = new Pelicula("El señor de los anillos", 2001);
+        peliculaDeBruno.evalua(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(miPelicula);
+        lista.add(otraPelicula);
+        lista.add(peliculaDeBruno);
+        lista.add(lost);
+
+        for (Titulo item: lista) {
+            System.out.println(item.getNombre());
+            if (item instanceof Pelicula pelicula && pelicula.getClasificacion() > 2) {
+                System.out.println(pelicula.getClasificacion());
+            }
+        }
+//        lista.forEach(item -> System.out.println(item));
+//        lista.forEach(System.out::println);
+
+        ArrayList<String> listaDeArtistas = new ArrayList<>();
+        listaDeArtistas.add("Penélope Cruz");
+        listaDeArtistas.add("Antonio Banderas");
+        listaDeArtistas.add("Ricardo Darín");
+        System.out.println(listaDeArtistas);
+
+        Collections.sort(listaDeArtistas);
+        System.out.println("Lista de artistas ordenada: " + listaDeArtistas);
+
+        Collections.sort(lista);
+        System.out.println("Lista de títulos ordenados: " + lista);
+
+        lista.sort(Comparator.comparing(Titulo::getFechaDeLanzamiento));
+        System.out.println("Lista ordenada por fecha: " + lista);
+
+    }
+}
